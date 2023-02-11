b4dfd6ea42bba99f02179e7c10eb4eb7b0411064
madan5897 <madanhs5897@gmail.com>
2023-02-11 21:22:22 +0530

    First Commit

--- script.js
+++ script.js
@@ -8,15 +8,32 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map((x) => {
+    if (x.marks > 50) {
+      console.log(x.name);
+    }
+  });
 }
+PrintStudentswithMap();
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach((x) => {
+    if (x.marks > 50) {
+      console.log(x.name);
+    }
+  });
 }
+PrintStudentsbyForEach();
 
+const newElement = { id: 4, name: "susan", age: "20", marks: 45 };
 function addData() {
   //Write your code here, just console.log
+  arr.push(newElement);
+  console.log(arr);
 }
+
+addData();
 
 function removeFailedStudent() {
   //Write your code here, just console.log
@@ -24,4 +41,13 @@ function removeFailedStudent() {
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr1 = [
+    { id: 4, name: "joe", age: "25", marks: 55 },
+    { id: 5, name: "joi", age: "22", marks: 60 },
+    { id: 6, name: "Dark", age: "26", marks: 62 },
+  ];
+  const combinedArr = arr.concat(arr1);
+  console.log(combinedArr);
 }
+
+concatenateArray();
